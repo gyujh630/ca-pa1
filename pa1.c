acb9e70efbfd3fb60a0303d6399d395e14c81afd
gyujh630 <rbwjrbxo@ajou.ac.kr>
2023-10-13 20:30:22 +0900

    Feat: registers mapping

--- pa1.c
+++ pa1.c
@@ -65,9 +65,19 @@ typedef unsigned char bool;
  */
 static unsigned int translate(int nr_tokens, char *tokens[])
 {
-	/* TODO:
-	 * This is an example MIPS instruction. You should change it accordingly.
-	 */
+	const char* registers[32] = {
+    "$zero", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3",
+    "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7",
+    "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7",
+    "$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$fp", "$ra"
+  };
+
+	printf("%s\n", registers[0]);
+
+	/* registers 배열 길이
+	int num_registers = sizeof(registers) / sizeof(registers[0]);
+  printf("%d\n", num_registers);
+	*/
 	return 0x02324020;
 }
 
