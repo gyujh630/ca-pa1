8078e428601eb189ba96a2dfee6b558e7c7d5951
gyujh630 <rbwjrbxo@ajou.ac.kr>
2023-10-13 21:32:43 +0900

    Feat: format별 처리

--- pa1.c
+++ pa1.c
@@ -25,16 +25,15 @@
 
 /*====================================================================*/
 /*          ****** DO NOT MODIFY ANYTHING FROM THIS LINE ******       */
-#define MAX_NR_TOKENS	16	/* Maximum length of tokens in a command */
-#define MAX_TOKEN_LEN	32	/* Maximum length of single token */
-#define MAX_ASSEMBLY	128 /* Maximum length of assembly string */
+#define MAX_NR_TOKENS 16 /* Maximum length of tokens in a command */
+#define MAX_TOKEN_LEN 32 /* Maximum length of single token */
+#define MAX_ASSEMBLY 128 /* Maximum length of assembly string */
 
 typedef unsigned char bool;
-#define true	1
-#define false	0
+#define true 1
+#define false 0
 /*          ****** DO NOT MODIFY ANYTHING UP TO THIS LINE ******      */
 /*====================================================================*/
-
 
 /***********************************************************************
  * translate()
@@ -63,25 +62,79 @@ typedef unsigned char bool;
  *   Return a 32-bit MIPS instruction
  *
  */
-static unsigned int translate(int nr_tokens, char *tokens[])
+
+/* table */
+const char *registers[32] = {
+		"$zero", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3",
+		"$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7",
+		"$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7",
+		"$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$fp", "$ra"};
+
+/* R-format */
+const char *r_instructions[] = {"add", "sub", "and", "or", "nor"};
+int r_opcode = 0; // 고정
+int r_funct[] = {0x20, 0x22, 0x24, 0x25, 0x27};
+
+/* R-Shift */
+const char *r_shift_instructions[] = {"sll", "srl", "sra"};
+int r_shift_funct[] = {0x00, 0x02, 0x03};
+
+/* I-format */
+const char *i_instructions[] = {"addi", "andi", "ori", "lw", "sw", "beq", "bne"};
+int i_opcodes[] = {0x08, 0x0c, 0x0d, 0x23, 0x2b, 0x04, 0x05};
+
+int detectInstructionType(const char *instruction)
 {
-	const char* registers[32] = {
-    "$zero", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3",
-    "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7",
-    "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7",
-    "$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$fp", "$ra"
-  };
+	for (int i = 0; i < 5; i++)
+	{
+		if (strcmp(instruction, r_instructions[i]) == 0)
+		{
+			return 0; // R-format
+		}
+	}
 
-	printf("%s\n", registers[0]);
+	for (int i = 0; i < 3; i++)
+	{
+		if (strcmp(instruction, r_shift_instructions[i]) == 0)
+		{
+			return 1; // R-Shift format
+		}
+	}
 
-	/* registers 배열 길이
-	int num_registers = sizeof(registers) / sizeof(registers[0]);
-  printf("%d\n", num_registers);
-	*/
-	return 0x02324020;
+	for (int i = 0; i < 7; i++)
+	{
+		if (strcmp(instruction, i_instructions[i]) == 0)
+		{
+			return 2; // I-format
+		}
+	}
+
+	return -1;
 }
 
+static unsigned int translate(int nr_tokens, char *tokens[])
+{
+	unsigned int code = 0x02324020;
+	int instruction_type = detectInstructionType(tokens[0]);
+	switch (instruction_type)
+	{
+	case 0:
+		printf("hi r");
+		break;
+	case 1:
+		printf("hi s");
+		break;
+	case 2:
+		printf("hi i");
+		break;
+	default:
+		// 에러 처리
+		printf("hi err");
+		break;
+	}
 
+	return code;
+}
 
 /***********************************************************************
  * parse_command()
@@ -115,12 +168,17 @@ static int parse_command(char *assembly, int *nr_tokens, char *tokens[])
 	int token_started = false;
 	*nr_tokens = 0;
 
-	while (*curr != '\0') {  
-		if (isspace(*curr)) {  
+	while (*curr != '\0')
+	{
+		if (isspace(*curr))
+		{
 			*curr = '\0';
 			token_started = false;
-		} else {
-			if (!token_started) {
+		}
+		else
+		{
+			if (!token_started)
+			{
 				tokens[*nr_tokens] = curr;
 				*nr_tokens += 1;
 				token_started = true;
@@ -132,27 +190,29 @@ static int parse_command(char *assembly, int *nr_tokens, char *tokens[])
 	return 0;
 }
 
-
 /*====================================================================*/
 /*          ****** DO NOT MODIFY ANYTHING BELOW THIS LINE ******      */
 
 /***********************************************************************
  * The main function of this program.
  */
-int main(int argc, char * const argv[])
+int main(int argc, char *const argv[])
 {
-	char assembly[MAX_ASSEMBLY] = { '\0' };
+	char assembly[MAX_ASSEMBLY] = {'\0'};
 	FILE *input = stdin;
 
-	if (argc > 1) {
+	if (argc > 1)
+	{
 		input = fopen(argv[1], "r");
-		if (!input) {
+		if (!input)
+		{
 			fprintf(stderr, "No input file %s\n", argv[0]);
 			return EXIT_FAILURE;
 		}
 	}
 
-	if (input == stdin) {
+	if (input == stdin)
+	{
 		printf("*********************************************************\n");
 		printf("*          >> SCE212 MIPS translator  v0.10 <<          *\n");
 		printf("*                                                       *\n");
@@ -168,12 +228,14 @@ int main(int argc, char * const argv[])
 		printf(">> ");
 	}
 
-	while (fgets(assembly, sizeof(assembly), input)) {
-		char *tokens[MAX_NR_TOKENS] = { NULL };
+	while (fgets(assembly, sizeof(assembly), input))
+	{
+		char *tokens[MAX_NR_TOKENS] = {NULL};
 		int nr_tokens = 0;
 		unsigned int instruction;
 
-		for (size_t i = 0; i < strlen(assembly); i++) {
+		for (size_t i = 0; i < strlen(assembly); i++)
+		{
 			assembly[i] = tolower(assembly[i]);
 		}
 
@@ -184,10 +246,12 @@ int main(int argc, char * const argv[])
 
 		fprintf(stderr, "0x%08x\n", instruction);
 
-		if (input == stdin) printf(">> ");
+		if (input == stdin)
+			printf(">> ");
 	}
 
-	if (input != stdin) fclose(input);
+	if (input != stdin)
+		fclose(input);
 
 	return EXIT_SUCCESS;
 }
